8a0b3a7d2393140a04c6ef0fae4d07e0398386f1
Maximo Silva <siruasor@gmail.com>
2022-11-14 22:32:36 -0500

    initial base-commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import {
 } from 'react-router-dom'
 
 import { Navbar } from './app/Navbar'
+import { AddPostForm } from './features/posts/AddPostForm'
+
+import { PostsList } from './features/posts/PostsList'
 
 function App() {
   return (
@@ -18,9 +21,10 @@ function App() {
             exact
             path="/"
             render={() => (
-              <section>
-                <h2>Welcome to the Redux Essentials example app!</h2>
-              </section>
+              <>
+                <AddPostForm />
+                <PostsList />
+              </>
             )}
           />
           <Redirect to="/" />
