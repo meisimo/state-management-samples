b263e3f0cd5a099cb0757cd2aa5719beef24b5f2
Maximo Silva <siruasor@gmail.com>
2022-11-14 23:13:47 -0500

    users and edit and single page

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,20 +1,69 @@
 import { createSlice } from '@reduxjs/toolkit'
+import { nanoid } from '@reduxjs/toolkit'
+import { sub } from 'date-fns'
+
+function timeAgo(minutes) {
+  return sub(new Date(), { minutes }).toISOString()
+}
+
+const basicPost = (id, title, content, minutesAgo) => ({
+  id,
+  title,
+  content,
+  date: timeAgo(minutesAgo),
+  reactions: {
+    thumbsUp: 0,
+    hooray: 0,
+    heart: 0,
+    rocket: 0,
+    eyes: 0,
+  },
+})
 
 const initialState = [
-  { id: '1', title: 'First Post!', content: 'Hello!' },
-  { id: '2', title: 'Second Post', content: 'More text' }
+  basicPost('1', 'First Post', 'Hello', 10),
+  basicPost('2', 'Second Post', 'More text', 5),
 ]
 
 const postsSlice = createSlice({
   name: 'posts',
   initialState,
   reducers: {
-    postAdded(state, action) {
-      state.push(action.payload)
+    postAdded: {
+      reducer(state, action) {
+        state.push(action.payload)
+      },
+      prepare(title, content, userId) {
+        return {
+          payload: {
+            id: nanoid(),
+            date: new Date().toISOString(),
+            title,
+            content,
+            user: userId,
+          }
+        }
+      }
+    },
+    postUpdated(state, action) {
+      const { id, title, content } = action.payload
+      const existingPost = state.find(post => post.id === id)
+      if (existingPost) {
+        existingPost.title = title
+        existingPost.content = content
+      }
+    },
+    reactionAdded(state, action) {
+      const { postId, reaction } = action.payload
+      const existingPost = state.find(post => post.id === postId)
+      if (existingPost) {
+        existingPost.reactions[reaction]++
+      }
     }
   }
 })
 
-export const { postAdded } = postsSlice.actions
+
+export const { postAdded, postUpdated, reactionAdded } = postsSlice.actions
 
 export default postsSlice.reducer
